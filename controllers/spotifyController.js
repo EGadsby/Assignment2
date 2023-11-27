cc621a40a2801ad64e77a06252e22b11a1c49230
Gareth Carvalho <garethcarva@gmail.com>
2023-11-26 22:24:33 -0700

    finished backend of activity

--- controllers/spotifyController.js
+++ controllers/spotifyController.js
@@ -4,7 +4,7 @@ let querystring = require('querystring')
 const axios = require('axios')
 const fs = require('fs')
 
-const StringHelper = require('../helpers/stringHelpers')
+const stringHelper = require('../helpers/stringHelpers');
 
 let client_id = process.env.client_id;
 let client_secret = process.env.client_secret;
@@ -12,111 +12,25 @@ var redirect_uri = 'http://localhost:8888/spotify/callback';
 
 
 const express = require('express');
-const router = express.Router(); // calling routes on api
+const app = express.Router(); // calling routes on api
 
 // PASTE THE LOGIN FUNCTION HERE
+app.get('/login', function(req, res) {
 
-
-
-// playlist getting function by parsing the bearer token from the bearer.json file 
-// test using ?page=1 param
-router.get('/playlists', function (req, res) {
-    var page = req.query.page || 1;
-
-    let offset = (page - 1) * 50// in case the user has more than 50 playlist (how cultured)
-
-    fs.readFile('bearer.json', 'utf8', (err, data) => { // Reads the information from the bearer file
-        if (err) { // Error handling if we cannot read from file for whatever reason
-            console.error('Error reading file:', err);
-            return;
-        }
-        try { // here we are parsing the data from the bearer.json file
-            let jsonData = JSON.parse(data);
-            let config = {
-                method: 'get',
-                maxBodyLength: Infinity,
-                url: `https://api.spotify.com/v1/me/playlists?offset=${offset}&limit=50`,// the url for the spotify playlist api
-                headers: {
-                    'Authorization': `Bearer ${jsonData.access_token}` // passing in the access token as a header
-                }
-            };
-
-            console.log(config)
-            axios.request(config) // similar to the callback endpoint, we are using axios to post the request async 
-                .then((response) => {
-                    console.log(JSON.stringify(response.data.items));
-                    res.send(response.data) // displaying the JSON response on screen
-                })
-                .catch((error) => {
-                    console.log(error); // error handling 
-                });
-        } catch (error) {
-            console.error('Error parsing JSON:', error);
-        }
-    })
+    var state = stringHelper.generateRandomString(16);
+    var scope = 'user-read-private user-read-email playlist-read-private playlist-read-collaborative';
+  
+    res.redirect('https://accounts.spotify.com/authorize?' +
+      querystring.stringify({
+        response_type: 'code',
+        client_id: client_id,
+        scope: scope,
+        redirect_uri: redirect_uri,
+        state: state
+      }));
 });
 
-router.get(`/playlists/:playlist_id/tracks`, async function (req, res) {
-    var page = req.query.page || 1;
-
-    let offset = (page - 1) * 100
-
-    var playlistid = req.params.playlist_id
-
-    // Try coding the answer here!
-    
-
-
-});
-
-// DO NOT GO BEYOND THIS POINT
-
-router.get('/auth', function (req, res) {
-    fs.readFile('bearer.json', 'utf8', (err, data) => {
-        if (err) {
-            console.error('Error reading file:', err);
-            return;
-        }
-        try {
-            let jsonData = JSON.parse(data);
-            res.send(jsonData);
-        } catch (error) {
-            console.error('Error parsing JSON:', error);
-        }
-    })
-});
-
-router.get('/me', function (req, res) {
-    fs.readFile('bearer.json', 'utf8', (err, data) => {
-        if (err) {
-            console.error('Error reading file:', err);
-            return;
-        }
-        try {
-            let jsonData = JSON.parse(data);
-            let config = {
-                method: 'get',
-                maxBodyLength: Infinity,
-                url: `https://api.spotify.com/v1/me`,// the url for the spotify playlist api
-                headers: {
-                    'Authorization': `Bearer ${jsonData.access_token}` // passing in the access token as a header
-                }
-            };
-
-            axios.request(config) // similar to the callback endpoint, we are using axios to post the request async 
-                .then((response) => {
-                    res.send(response)
-                })
-                .catch((error) => {
-                    console.log(error); // error handling 
-                });
-        } catch (error) {
-            console.error('Error parsing JSON:', error);
-        }
-    })
-});
-
-router.get('/callback', function (req, res) {
+app.get('/callback', function (req, res) {
     // The below code we get directly from the spotify api documentation
     var code = req.query.code || null;
     var state = req.query.state || null;
@@ -158,4 +72,133 @@ router.get('/callback', function (req, res) {
     }
 });
 
-module.exports = router; 
+// playlist getting function by parsing the bearer token from the bearer.json file 
+// test using ?page=1 param
+app.get('/playlists', function (req, res) {
+    var page = req.query.page || 1;
+
+    let offset = (page - 1) * 50// in case the user has more than 50 playlist (how cultured)
+
+    fs.readFile('bearer.json', 'utf8', (err, data) => { // Reads the information from the bearer file
+        if (err) { // Error handling if we cannot read from file for whatever reason
+            console.error('Error reading file:', err);
+            return;
+        }
+        try { // here we are parsing the data from the bearer.json file
+            let jsonData = JSON.parse(data);
+            let config = {
+                method: 'get',
+                maxBodyLength: Infinity,
+                url: `https://api.spotify.com/v1/me/playlists?offset=${offset}&limit=50`,// the url for the spotify playlist api
+                headers: {
+                    'Authorization': `Bearer ${jsonData.access_token}` // passing in the access token as a header
+                }
+            };
+
+            console.log(config)
+            axios.request(config) // similar to the callback endpoint, we are using axios to post the request async 
+                .then((response) => {
+                    console.log(JSON.stringify(response.data.items));
+                    res.send(response.data) // displaying the JSON response on screen
+                })
+                .catch((error) => {
+                    console.log(error); // error handling 
+                });
+        } catch (error) {
+            console.error('Error parsing JSON:', error);
+        }
+    })
+});
+
+app.get(`/playlists/:playlist_id/tracks`, async function (req, res) {
+    var page = req.query.page || 1;
+
+    let offset = (page - 1) * 100
+
+    var playlistId = req.params.playlist_id
+
+    // Try coding the answer here!
+    fs.readFile('bearer.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading file: ', err);
+            return;
+        }
+
+        try {
+            let jsonData = JSON.parse(data);
+            let config = {
+                method: 'GET',
+                maxBodyLength: Infinity,
+                url: `https://api.spotify.com/v1/playlists/${playlistId}/tracks`,
+                headers: {
+                    'Authorization': `Bearer ${jsonData.access_token}`
+                }
+            };
+
+            axios.request(config)
+            .then((response) => {
+                const items = response.data.items;
+                res.send(items.map(item => ({
+                    id: item.track.external_ids.isrc,
+                    name: item.track.name,
+                    artists: item.track.artists,
+                    images: item.track.album.images,
+                    durationMs: item.track.duration_ms
+                })));
+            })
+        } catch(err) {
+            throw (err);
+        }
+    })
+
+
+});
+
+// DO NOT GO BEYOND THIS POINT
+
+app.get('/auth', function (req, res) {
+    fs.readFile('bearer.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading file:', err);
+            return;
+        }
+        try {
+            let jsonData = JSON.parse(data);
+            res.send(jsonData);
+        } catch (error) {
+            console.error('Error parsing JSON:', error);
+        }
+    })
+});
+
+app.get('/me', function (req, res) {
+    fs.readFile('bearer.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading file:', err);
+            return;
+        }
+        try {
+            let jsonData = JSON.parse(data);
+            let config = {
+                method: 'get',
+                maxBodyLength: Infinity,
+                url: `https://api.spotify.com/v1/me`,// the url for the spotify playlist api
+                headers: {
+                    'Authorization': `Bearer ${jsonData.access_token}` // passing in the access token as a header
+                }
+            };
+
+            axios.request(config) // similar to the callback endpoint, we are using axios to post the request async 
+                .then((response) => {
+                    res.send(response)
+                })
+                .catch((error) => {
+                    console.log(error); // error handling 
+                });
+        } catch (error) {
+            console.error('Error parsing JSON:', error);
+        }
+    })
+});
+
+module.exports = app; 
